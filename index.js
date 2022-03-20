d773e222a0192829a10d9a94d3d132ef27e10afb
Johnathan <jocuog@gmail.com>
2022-03-19 18:01:31 -0600

    commit tree 9985ce66c36591216dd648c1766ab9f62c2cd112

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(arr, str) {
+    const newStr = str.toUpperCase()
+    const filtered = arr.filter(el => {
+        return el.toUpperCase() === newStr})
+        return filtered
+}
+
+
+console.log(findMatching(drivers, "Bobby"))
+
+function fuzzyMatch(arr, str) {
+    const newStr2 = str.charAt(0)
+    const fuzzy = arr.filter(el => {
+        return el.charAt(0) === newStr2})
+        return fuzzy
+}
+
+function matchName(arr, str) {
+    const match = arr.filter(el => {
+        return el.name === str
+    })
+    return match
+}
+
